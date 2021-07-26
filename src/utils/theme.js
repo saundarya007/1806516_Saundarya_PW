a4b3d41ab8c31d401baf6e021584c7601c6ede27
Saundarya <77521327+saundarya007@users.noreply.github.com>
2021-07-26 16:26:31 +0530

    Add files via upload

---
+++ src/utils/theme.js
@@ -0,0 +1,15 @@
+    
+/**
+ * @param px input px as per 1920 * 1080 resolution
+ */
+export const pxToVw = px => `${px * 0.05208}vw`;
+
+/**
+ * @param px input px as per 1920 * 1080 resolution
+ */
+export const pxToVh = px => `${px * 0.09259}vh`;
+
+/**
+ * @param px input px as per 1920 * 1080 resolution
+ */
+export const pxToRem = px => `${px / 22.5}rem`;
